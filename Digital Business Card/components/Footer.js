fbeb0c2722e8feb94df288f09aef4b537d940df3
Aman Singh <61135773+iamamyaman@users.noreply.github.com>
2022-02-16 20:17:24 +0530

    Create Footer.js

---
+++ Digital Business Card/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+
+export default function Footer(){
+    return(
+        <div className = "social-icons">
+            <i className="fa fa-twitter"></i>
+            <i className="fa fa-facebook"></i>
+            <i className="fa fa-instagram"></i>
+            <i className="fa fa-github"></i> 
+        </div>
+        
+    )
+}
